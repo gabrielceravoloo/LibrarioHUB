e8f9a34f0acc5b4f84ea8d9202d9badf8e6cd4ac
gabrielceravoloo <gabriel.sceravolo26@gmail.com>
2024-07-25 00:23:07 -0300

    TESTE - Criando os primeiros testes de rotas e views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,13 @@
 exports.home = (req, res) => {
-    res.render('index', { title: 'Inicio' });
+    res.render('index', {
+        title: 'Pagina Inicial',
+        content: '<p>Conteúdo dinâmico para a página inicial.</p>'
+    });
 };
 
-exports.home = (req, res) => {
-    res.render('sobre', { title: 'Sobre' });
+exports.sobre = (req, res) => {
+    res.render('sobre', {
+        title: 'Sobre',
+        content: '<p>Conteúdo dinâmico sobre nós.</p>'
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.home);
+router.get('/sobre', homeController.sobre);
 
 module.exports = router;
